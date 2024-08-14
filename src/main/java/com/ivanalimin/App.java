d6583165de39c64929633e8301ac820b32fdcd9a
Ivan Alimin <aliminivan1990@gmail.com>
2024-08-14 19:44:42 +0300

    Work with LocalDateTime using @JsonSerialize and custom Serializer - add task description

--- src/main/java/com/ivanalimin/App.java
+++ src/main/java/com/ivanalimin/App.java
@@ -5,6 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
 
+/*
+Работа с датами одна из самых частовстречающихся задач в работе. Уделите этой теме должное внимание.
+Для тренировки предлогаю вам задачу, на вход вам дается время в виде класса LocalDateTime, орисуйте его в виде:
+
+год:месяц:день##:час:минут:секунды:милисекунды
+
+Используйте для этого аннотации jackson в спринге. И не забудьте про локаль.
+ */
+
 public class App {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
